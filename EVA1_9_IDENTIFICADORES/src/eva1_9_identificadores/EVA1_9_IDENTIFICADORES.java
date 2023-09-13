eebf6e4f0334b5620dfb65bd09c76295ecb777f6
Carlos Cerda <143562904+futuroinge@users.noreply.github.com>
2023-09-13 17:53:50 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_9_identificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // int 1nombre; nombre no valido, empieza con un numero
+        int $nombre;
+        int _nombre;
+        int nombr3; 
+        int NOMBRE;
+        int Nombre; 
+        int nombre; 
+        /*int NOMBRE;*/
+        
+        int Public;
+        // LA DECLARACION TIENE COMO OBJETIVO DELIMITAR LAS PROPIEDADES DE UN IDENTIFICADOR
+    }
+    
+}
